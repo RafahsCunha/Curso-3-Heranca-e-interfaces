e07c2384421cdab44d429ce9e639b10bcb9851a7
RafahsCunha <rafaelhsc20@gmail.com>
2023-01-08 20:13:56 -0300

    Diretorio e classe SistemaInterno

---
+++ Usando Heranca e Implementando Interfaces/SistemaInterno/SistemaInterno.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Usando_Heranca_e_Implementando_Interfaces.Funcionarios;
+
+namespace Usando_Heranca_e_Implementando_Interfaces.SistemaInterno
+{
+    public class SistemaInterno
+    {
+        public bool logar(Diretor funcionario, string senha)
+        {
+            bool usuarioAutenticado = funcionario.autenticar(senha);
+            if (usuarioAutenticado)
+            {
+                Console.WriteLine("Usuario Logado. Bem vindo ao sistema!");
+                return true;
+            }
+            else
+            {
+                
+                Console.WriteLine("Senha incorreta!");
+                return false;
+            }
+
+        }
+        public bool logar(GerenteDeContas funcionario, string senha)
+        {
+            bool usuarioAutenticado = funcionario.autenticar(senha);
+            if (usuarioAutenticado)
+            {
+                Console.WriteLine("Usuario Logado. Bem vindo ao sistema!");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Senha incorreta!");
+                return false;
+            }
+        }
+    }
+}
